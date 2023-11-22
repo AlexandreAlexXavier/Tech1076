0ad17322b28c3f500d2fade176fe941a8037a561
ALEXANDRE ALEX BARBOSA XAVIER <50221711+AlexandreAlexXavier@users.noreply.github.com>
2023-11-22 20:00:02 -0300

    Nova versão bingo

--- src/Bingo.java
+++ src/Bingo.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
+
+import static java.lang.Integer.compare;
+import static java.lang.Integer.valueOf;
 
 public class Bingo {
 
@@ -62,39 +66,96 @@ public class Bingo {
         int valorMaximoNaCartela = 60;
         int numeroDeJogadores = nomesDeJogadores.length;
 
-        int[][] cartelasGeradas = GerarSequenciaDeNumeros(numeroDeJogadores,quantidadeDeNumerosPorCartela,
+        int[][] cartelasGeradas = GerarSequenciaDeNumeros(numeroDeJogadores, quantidadeDeNumerosPorCartela,
                 valorMaximoNaCartela);
 
-        for (int i = 0; i < nomesDeJogadores.length; i++) {
+        /*for (int i = 0; i < nomesDeJogadores.length; i++) {
             System.out.printf(nomesDeJogadores[i].toUpperCase() + " " + Arrays.toString(cartelasGeradas[i]));
             System.out.println();
-        }
+        }*/
 
         int[] saquinhoDeSorteio = CriarListaDeNumeros(valorMaximoNaCartela);
-        int[][] numerosSorteados = GerarSequenciaDeNumeros(1,quantidadeDeNumerosPorCartela,
-                valorMaximoNaCartela);
-
         System.out.println(Arrays.toString(saquinhoDeSorteio));
         System.out.println();
-        System.out.println(Arrays.toString(numerosSorteados[0]));
-        System.out.println();
+
+        int[][] numeroDeAcertos = new int[numeroDeJogadores][5];
+        int numeroDePosicoes = saquinhoDeSorteio.length;
 
 
-        int[] saquinhoAuxiliar = new int[55];
-        int j=0;
-        int k=0;
+        for (int round = 1; round < 13; round++) {
 
-        for ( int i = 0;i < saquinhoDeSorteio.length-1; i++) {
-            if (j<5 && i == numerosSorteados[0][j]) {
-                j++;
-            } else {
-                saquinhoAuxiliar[k] = saquinhoDeSorteio[i];
-                k++;
+            int[][] posicoesSorteadas = GerarSequenciaDeNumeros(1, quantidadeDeNumerosPorCartela,
+                    numeroDePosicoes);
+            int[]   numerosSorteados = new int[5];
+
+            for (int i=0; i<5; i++) {
+                numerosSorteados[i] = saquinhoDeSorteio[posicoesSorteadas[0][i]];
+                }
+            System.out.println();
+            System.out.println("Round: " + round +
+                                " Números sorteados: " + Arrays.toString(numerosSorteados));
+            System.out.println();
+
+            for (int a = 0; a < numeroDeJogadores; a++) {
+                for (int b=0; b<5; b++){
+                    for (int c=0; c<5; c++) {
+                        if (numerosSorteados[b] == cartelasGeradas[a][c]) {
+                            numeroDeAcertos[a][c] = 1;
+                        }
+                    }
+                }
             }
 
+            for (int i = 0; i < numeroDeJogadores; i++) {
+                System.out.printf(nomesDeJogadores[i].toUpperCase() +
+                        " " + Arrays.toString(cartelasGeradas[i]) +
+                        " " + Arrays.toString(numeroDeAcertos[i]));
+                System.out.println();
+            }
+            int numeroDePosicoesAux = numeroDePosicoes;
+            numeroDePosicoes = numeroDePosicoes - 5;
+
+            int[] saquinhoAuxiliar = new int[numeroDePosicoes];
+            int j = 0;
+            int k = 0;
+
+            for (int i = 0; i < numeroDePosicoesAux; i++) {
+                if (j < 5 && i == posicoesSorteadas[0][j]) {
+                    j++;
+                } else {
+                    saquinhoAuxiliar[k] = saquinhoDeSorteio[i];
+                    k++;
+                }
+
+            }
+            for (int i = 0; i < numeroDePosicoes; i++) {
+                saquinhoDeSorteio[i] = saquinhoAuxiliar[i];
+            }
+
+            numeroDePosicoes = saquinhoAuxiliar.length;
+
+            /*
+            System.out.println(Arrays.toString(saquinhoDeSorteio));
+            System.out.println(Arrays.toString(saquinhoAuxiliar));
+            System.out.println();
+            */
+
+            int[] total = new int[numeroDeJogadores];
+            boolean alguemVenceu = false;
+
+            for (int d =0; d<numeroDeJogadores; d++){
+                for (int e = 0; e<5; e++){
+                    total[d] = total[d] + numeroDeAcertos[d][e];
+                    if (total[d] == 5) {
+                        alguemVenceu = true;
+                    }
+
+                }
+            }
+            if (alguemVenceu){
+                System.out.println("Alguem venceu");
+                break;
+            }
         }
-
-
-        System.out.println(Arrays.toString(saquinhoAuxiliar));
     }
 }
